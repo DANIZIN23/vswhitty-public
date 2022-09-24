c1efa97d9c89803cb3c886c7f6a66b0dbe249c9a
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-09-23 22:38:22 -0300

    Update Ratings.hx

--- source/Ratings.hx
+++ source/Ratings.hx
@@ -133,7 +133,7 @@ class Ratings
     {
         return 
         (FlxG.save.data.npsDisplay ? "NPS: " + nps + (!FlxG.save.data.botplay ? " | " : "") : "") + (!FlxG.save.data.botplay ?	// NPS Toggle
-        "Score:" + (Conductor.safeFrames != 10 ? score + " (" + scoreDef + ")" : "" + score) + 									// Score
+        "Port By Danizin|Score:" + (Conductor.safeFrames != 10 ? score + " (" + scoreDef + ")" : "" + score) + 									// Score
         " | Combo Breaks:" + PlayState.misses + 																				// Misses/Combo Breaks
         " | Accuracy:" + (FlxG.save.data.botplay ? "N/A" : HelperFunctions.truncateFloat(accuracy, 2) + " %") +  				// Accuracy
         " | " + GenerateLetterRank(accuracy) : ""); 																			// Letter Rank
