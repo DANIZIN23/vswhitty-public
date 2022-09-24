2d1ff4e659bb083d7c5de16da0793e527d9b1472
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-09-23 22:48:50 -0300

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -134,7 +134,11 @@ class FreeplayState extends MusicBeatState
 			trace(md);
 		 */
 
-		super.create();
+		#if android
+		addVirtualPad(LEFT_FULL, A_B);
+		#end	
+		
+			super.create();
 	}
 
 	public function addSong(songName:String, weekNum:Int, songCharacter:String)
